40c4b901e4d28ea26568356f5b59f5b48fd5c3f1
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com~>
2025-08-22 10:37:20 -0700

    TypeScript Tutorial #11 - Array Data Type

---
+++ TypeScript Tutorial #11 - Array Data Type/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("TypeScript Tutorial #11: Array Data Type");
+// Arrays are the collection of elements of the same type.
+// There are 2 conventions to define an array in TypeScript:
+// 1. Using the type followed by square brackets,
+let users = ["Faiq", "Ali", "Talha", "Hasan"];
+let ids = [1, 2, 3, 4, 5];
+// 2. Using the Array generic type,
+let students = ["Sohaib", "Huzaifa1", "Huzaifa2", "Saif"];
+let marks = [10, 20, 30, 40, 50];
+// ReadOnly Arrays and their use case:
+marks[0] = 100; // This is valid
+console.log("Marks before ReadOnly:", marks);
+let finalMarks = [...marks];
+// finalMarks[0] = 25; // This will throw an error because finalMarks
+console.log("Final Marks after ReadOnly:", finalMarks);
